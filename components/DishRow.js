3b94e1f17fc617c247f5a6ed0707f799e98c3e54
JavaidAhmadKhan <relaxjavaid@gmail.com>
2023-01-30 01:38:28 +0530

    added condtional rendering

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,13 +1,52 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import Currency from "react-currency-formatter";
+import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+
+import { urlFor } from "../sanity";
 
 const DishRow = ({ id, name, description, price, image }) => {
+  const [isPressed, setIsPressed] = useState(false);
+
   return (
-    <TouchableOpacity>
-      <View>
-        <Text>{name}</Text>
-        <Text>{description}</Text>
-      </View>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        className={`bg-white border p-4 border-gray-200 ${
+          isPressed && "border-b-0"
+        }`}
+      >
+        <View className="flex-row">
+          <View className="flex-1 pr-2">
+            <Text className="text-lg mb-1">{name}</Text>
+            <Text className="text-gray-400">{description}</Text>
+            <Text className="text-gray-400 mt-2">
+              <Currency quantity={price} currency="INR" />
+            </Text>
+          </View>
+          <View>
+            <Image
+              style={{ borderWidth: 1, borderColor: "#F3F3F4" }}
+              source={{ uri: urlFor(image).url() }}
+              className="h-20 w-20 bg-gray-300 p-4"
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3 ">
+            <TouchableOpacity>
+              <AntDesign name="minuscircleo" size={30} color="red" />
+            </TouchableOpacity>
+            <Text>0</Text>
+            <TouchableOpacity>
+              <AntDesign name="pluscircleo" size={30} color="#00CCBB" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </>
   );
 };
 
