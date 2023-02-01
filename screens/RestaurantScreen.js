3b0b452fc3dd2c0252eb819514ee876c8e68227e
JavaidAhmadKhan <relaxjavaid@gmail.com>
2023-02-01 23:35:08 +0530

    added BasketIcon Component

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -6,6 +6,7 @@ import { Entypo } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { urlFor } from "../sanity";
 import DishRow from "../components/DishRow";
+import BasketIcon from "../components/BasketIcon";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -31,81 +32,84 @@ const RestaurantScreen = () => {
   }, []);
 
   return (
-    <ScrollView>
-      <View className="relative">
-        <Image
-          source={{ uri: urlFor(imgUrl).url() }}
-          className="w-full h-56 bg-gray-300 p-4"
-        />
-        <TouchableOpacity
-          onPress={navigation.goBack}
-          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
-        >
-          <AntDesign name="arrowleft" size={24} color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
-      <View className="bg-white">
-        <View className="px-4 pt-4">
-          <Text className="text-3xl font-bold">{title}</Text>
-          <View className="flex-row space-x-2 my-1">
-            <View className="flex-row items-center space-x-1">
-              <Ionicons
-                name="ios-star"
-                size={20}
-                color="#00CCBB"
-                opacity={0.5}
-              />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-green-500">{rating}</Text> . {genre}
-              </Text>
-            </View>
-            <View className="flex-row items-center space-x-1">
-              <Ionicons
-                name="location-outline"
-                size={20}
-                opacity={0.4}
-                color="#00CCBB"
-              />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-gray-500">Nearby . {address}</Text>
-              </Text>
-            </View>
-          </View>
-          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+    <>
+      <BasketIcon />
+      <ScrollView>
+        <View className="relative">
+          <Image
+            source={{ uri: urlFor(imgUrl).url() }}
+            className="w-full h-56 bg-gray-300 p-4"
+          />
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+          >
+            <AntDesign name="arrowleft" size={24} color="#00CCBB" />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-200">
-          <AntDesign
-            name="questioncircleo"
-            size={20}
-            opacity={0.6}
-            color="#00CCBB"
-          />
-          <Text className="pl-2 flex-1 text-md font-bold">
-            Have a food allergy?
-          </Text>
-          <Entypo
-            name="chevron-right"
-            size={20}
-            color="#00CCBB"
-            opacity={0.5}
-          />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
-        {/* Dish rows */}
-        {dishes.map((dish) => (
-          <DishRow
-            key={dish._id}
-            id={dish._id}
-            name={dish.name}
-            description={dish.short_description}
-            price={dish.price}
-            image={dish.image}
-          />
-        ))}
-      </View>
-    </ScrollView>
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <Ionicons
+                  name="ios-star"
+                  size={20}
+                  color="#00CCBB"
+                  opacity={0.5}
+                />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-500">{rating}</Text> . {genre}
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1">
+                <Ionicons
+                  name="location-outline"
+                  size={20}
+                  opacity={0.4}
+                  color="#00CCBB"
+                />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-gray-500">Nearby . {address}</Text>
+                </Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+          </View>
+          <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-200">
+            <AntDesign
+              name="questioncircleo"
+              size={20}
+              opacity={0.6}
+              color="#00CCBB"
+            />
+            <Text className="pl-2 flex-1 text-md font-bold">
+              Have a food allergy?
+            </Text>
+            <Entypo
+              name="chevron-right"
+              size={20}
+              color="#00CCBB"
+              opacity={0.5}
+            />
+          </TouchableOpacity>
+        </View>
+        <View className="pb-36">
+          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+          {/* Dish rows */}
+          {dishes.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
