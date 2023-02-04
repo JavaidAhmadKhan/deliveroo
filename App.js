e3705fdbc04e5d5d5d028bd84319e6409ae7d61a
JavaidAhmadKhan <relaxjavaid@gmail.com>
2023-02-05 01:29:34 +0530

    added Preparing order Screen

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import BasketScreen from "./screens/BasketScreen";
+import PreparingOrderScreen from "./screens/PreparingOrderScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +21,11 @@ export default function App() {
             name="Basket"
             component={BasketScreen}
             options={{ presentation: "modal", headerShown: false }}
+          />
+          <Stack.Screen
+            name="PreparingOrderScreen"
+            component={PreparingOrderScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }}
           />
         </Stack.Navigator>
       </Provider>
---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,17 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const PreparingOrderScreen = () => {
+  return (
+    <SafeAreaView
+      style={{
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
+      className="bg-[#00CCBB] flex-1 justify-center items-center"
+    >
+      <Text>PreparingOrderScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PreparingOrderScreen;
